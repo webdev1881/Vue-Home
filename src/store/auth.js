d1d6c73336b074d8e566c792f6dde9ce24b43f3e
webdev1881 <webdev1881@gmail.com>
2019-10-05 23:01:43 +0300

    12

--- src/store/auth.js
+++ src/store/auth.js
@@ -28,7 +28,6 @@ export default {
 
         async loginPhone({ dispatch, commit }, phNo) {
 
-
             window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
 
             const appVerifier = window.recaptchaVerifier;
